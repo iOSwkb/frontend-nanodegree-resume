524b51adb04fa8e84d4cb0c85eda8ea459237dd2
JK210 <18346668775m0@sina.cn>
2017-11-23 15:31:50 +0800

    提交最新

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,51 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+var work = {
+	"jobs": [{
+		"employer": "ma yun",
+		"title": "a li",
+		"location": "hang zhou",
+		"dates": "2015",
+		"description": "ma yi cai fu"
+	},
+	{
+		"employer": "ma hua teng",
+		"title": "teng xun",
+		"location": "shang hai",
+		"dates": "2017",
+		"description": "wang zhe rong yao"
+	}]
+}
+
+var projects = {
+	"projects": [{
+		"title": "yi pu",
+		"dates": "2015",
+		"description": "er shou fang",
+		"images": ["images/fry.jpg","images/197x148.gif"]
+	}]
+}
+
+var bio = {
+	"name": "jane",
+	"role": "HTML",
+	"welcomeMessage": "Hello",
+	"biopic": "shenme",
+	"contacts": {
+		"mobile": "13735822994",
+		"email": "614034542@qq.com",
+		"github": "wkb",
+		"twitter": "meiyou",
+		"location": "huang di cheng"
+	}
+	"skills": ["js", "OC", "swift"]
+}
+
+var education = {
+	"onlineCourses": [{
+		"title": "benke",
+		"school": "dongbeishiyoudaxue",
+		"dates": "2012-2016",
+		"url": "www.baidu.com"
+	}
+	]
+}
